6ba2ead07ee6e5c3089180941054865e7203585b
CodyBrewer <codybrewerdev@gmail.com>
2020-04-25 15:59:08 -0600

    set up middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
-const app = express();
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express;
+
+// Global Middleware
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
 
 module.exports = app;
 
