c971cc8a65e5f3995bb6c16ace2bbc29a98910de
CodyBrewer <codybrewerdev@gmail.com>
2020-04-25 17:18:34 -0600

    create users tables

---
+++ data/migrations/20200425165059_create_users_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+	// auto incrementing userid
+	table.increments('userid').primary();
+	// username
+        table.text('username').notNullable();
+        // password
+	table.text('password', 8).notNullable();
+	// user roleid
+	table.integer('roleid').defaultTo(0);
+	// admin
+	table.integer('admin').defaultTo(0);
+	
+    });   
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+};
