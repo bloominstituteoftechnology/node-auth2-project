8e33b77473ad383b54508769c6b4df6ece850750
CodyBrewer <codybrewerdev@gmail.com>
2020-04-25 16:07:04 -0600

    changed api/users/model.js to router.js fO

--- api/server.js
+++ api/server.js
@@ -3,12 +3,17 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const usersRouter = require('./users/router');
+
 const app = express();
 
 // Global Middleware
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
+
+// Routers
+app.use('/api/users', usersRouter);
 
 module.exports = app;
 
