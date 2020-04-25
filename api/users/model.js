b8ab319c86fdc0101a03b9d59b0a5430ecbfe391
CodyBrewer <codybrewerdev@gmail.com>
2020-04-25 16:30:03 -0600

    scaffold functions for users/model

---
+++ api/users/model.js
@@ -0,0 +1,21 @@
+function get() {};
+
+function getBy(filter) {};
+
+function add(user) {};
+
+function remove(userid) {};
+
+function update(userid, changes) {};
+
+function patch(userid, changes) {};
+
+module.exports = {
+    get,
+    getBy,
+    add, 
+    remove, 
+    update, 
+    patch,
+};
+
