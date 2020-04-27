523004cb9a0ce9668c4516b1b61b7efee41a5bd7
CodyBrewer <codybrewerdev@gmail.com>
2020-04-27 16:27:17 -0600

    /api/auth/login and /register endpoints return jwt token

--- api/auth/router.js
+++ api/auth/router.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const bcrypt = require('bcryptjs');
 
+const { genToken } = require('../../utils/index.js');
+
 const Users = require('../users/model.js');
 
 router.get('/status', (req, res) => {
-  res.send({ authentication: 'up'});
+  res.send({ authentication: 'up' });
 });
 
 router.post('/register', async (req, res) => {
@@ -15,14 +17,31 @@ router.post('/register', async (req, res) => {
   user.password = hash;
 
   try {
-    const saved = await Users.add(user);
-    if (saved) {
-      res.status(201).json({ registeredUser: saved });
+    const registered = await Users.add(user);
+    if (registered) {
+      const token = genToken(registered);
+      res.status(201).json({ registered: registered.username, token });
     } else {
       res.status(401).json({ message: 'missing information' });
     }
   } catch (err) {
     res.status(500).json({ message: 'error', error: err.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const loggingIn = await Users.getBy({ username }).first();
+    if (loggingIn && bcrypt.compareSync(password, loggingIn.password)) {
+      const token = genToken({ username, password });
+      res.status(200).json({ message: `Welcome ${loggingIn.username}`, token });
+    } else {
+      res.status(401).json({ message: 'invalid credentials' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'server error', error: err.message });
   }
 });
 
