77956a46211cbfb1eb3fb58904bf26393cf9fd04
CodyBrewer <codybrewerdev@gmail.com>
2020-04-27 16:25:15 -0600

    changed to just grab the authorization header

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -3,15 +3,13 @@ const secret = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-
+    const token = req.headers.authorization;
     if (token) {
       jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
         if (err) {
           res.status(401).json({ message: 'forbidden' });
         } else {
           req.decodedJwt = decodedToken;
-          console.log(req.decodedJwt);
           next();
         }
       });
