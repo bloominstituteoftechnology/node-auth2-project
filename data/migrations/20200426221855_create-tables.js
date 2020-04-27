a21df05bda774f751741b449aaeedddcd5ffd140
BenHall-7 <benjaminjahall@gmail.com>
2020-04-26 23:17:28 -0400

    Setup

---
+++ data/migrations/20200426221855_create-tables.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl.string("username").notNullable().unique();
+    tbl.string("password").notNullable();
+    tbl.string("department").notNullable
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
+};
