be6a6752fb01d25d1f35ef0d19de93b7ebaa9e3b
Wangdi Tenzing <wangdi457@gmail.com>
2020-04-27 20:24:10 -0400

    Initialized npm, added dependencies such as cors, helmet, morgan , knex , nodemon and sqlite to start the server Checked the endpoint to see if the server was working Need to start making migrations now

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+  });
+  
+  module.exports = server;
