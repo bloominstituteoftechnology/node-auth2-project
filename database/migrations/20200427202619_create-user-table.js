b89a367e3c30a8b0eca7bf2da722aa470a1d4d31
Wangdi Tenzing <wangdi457@gmail.com>
2020-04-27 20:31:44 -0400

    Created a knex file , wrote knex migrations to create a table , Also created the database

---
+++ database/migrations/20200427202619_create-user-table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', users=>{
+      users.increments()
+      users
+      .string("username",123)
+      .notNullable()
+      .unique()
+      users.string('password',123).notNullable()
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users')
+};
