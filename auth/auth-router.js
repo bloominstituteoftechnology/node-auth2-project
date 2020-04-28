e2ba24588a1fbdcc43f88d75f252ffe7e685930a
Wangdi Tenzing <wangdi457@gmail.com>
2020-04-28 13:45:58 -0400

    Created a function to generate the token , able to generate one when i login Just realized that i also need a department key inside my users migration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt=require('jsonwebtoken')
 
 
 const Users = require("../users/user-model");
@@ -26,8 +27,10 @@ router.post("/login", (req,res)=>{
     .first()
     .then(user=>{
         if(user && bcrypt.compareSync(password,user.password)){
+            const token=generateToken(user)
             res.status(200).json({
-                message:`Welcome ${user.username}`
+                message:`Welcome ${user.username}`,
+                token,
             })
 
         }else{
@@ -38,5 +41,20 @@ router.post("/login", (req,res)=>{
         res.status(500).json(error)
     })
 })
+
+function generateToken(user){
+
+    const payload={
+        username:user.username
+    }
+
+ const secret="You should know this"
+
+ const options={
+  expiresIn:'1h',
+ }
+
+return jwt.sign(payload,secret,options)
+}
 
 module.exports=router
