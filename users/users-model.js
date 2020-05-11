aee31b698d0bb4dda33c67b86f8335155cdd8575
JC8747 <jonathan.chiaverini@gmail.com>
2020-05-10 20:28:15 -0400

    restored users folder

---
+++ users/users-model.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcryptjs");
+const db = require("../database/config");
+
+async function add(user) {
+  user.password = await bcrypt.hash(user.password, 14);
+
+  const [id] = await db("users").insert(user);
+  return findById(id);
+}
+
+function find() {
+  return db("users").select("id", "username", "department");
+}
+
+function findBy(filter) {
+  return db("users")
+    .select("id", "username", "password", "department")
+    .where(filter);
+}
+
+function findById(id) {
+  return db("users")
+    .select("id", "username")
+    .where({ id })
+    .first();
+}
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
