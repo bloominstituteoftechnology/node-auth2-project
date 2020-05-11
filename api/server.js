9c22f584093474f95dc101835bf8694aa02fad87
Your name <a.m.verdine@gmail.com>
2020-05-11 16:29:40 -0400

    Create and connect users-router & users-model

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const usersRouter = require("../users/users-router");
 
 const server = express();
 
 server.use(express.json());
+server.use("/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h1>Welcome</h1>`);
