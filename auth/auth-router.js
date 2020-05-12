de65961b25efeab2bdde0e791f4abe698f415c5a
Michael Lowe <mrlowememphis@yahoo.com>
2020-05-11 23:43:26 -0500

    routers created

---
+++ auth/auth-router.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
+const Users = require("../users/users-model")
+const restrict = require("../middleware/restrict")
+
+const router = express.Router()
+
+router.post("/register", async (req, res, next) => {
+	try {
+		const { username } = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		res.status(201).json(await Users.add(req.body))
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.post("/login", async (req, res, next) => {
+	try {
+		const { username, password } = req.body
+		const user = await Users.findBy({ username }).first()
+		const passwordValid = await bcrypt.compare(password, user.password)
+
+		if (!user || !passwordValid) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+		const payload = {
+			userId: user.id,
+			userRole: "normal", // this would normally come from a database
+		}
+		const token = jwt.sign(payload, process.env.JWT_SECRET)
+
+		res.cookie("token", token)
+		res.json({
+			message: `Welcome ${user.username}!`,
+		})
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.get("/logout", restrict(), (req, res, next) => {
+	// destroy the session here
+	res.end()
+})
+
+module.exports = router
