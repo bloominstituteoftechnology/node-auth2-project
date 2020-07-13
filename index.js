28539a706d4d0ae9e8de39420ea94f6b3f57f51a
Andrew Haberman <a.haberman25@gmail.com>
2020-07-13 15:14:58 -0400

    set up server / files

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+// const session = require("express-session")
+// const KnexSessionStore = require("connect-session-knex")(session)
+// const db = require("./data/config")
+// const usersRouter = require("./users/router")
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+// server.use(session({
+// 	resave: false, // avoids recreating sessions that have not changed
+// 	saveUninitialized: false, // comply with GDPR laws
+// 	secret: "rawrrrr",
+// 	store: new KnexSessionStore({
+// 		knex: db, // configured instance of Knex, or the live database connection
+// 		createtable: true, // if the session table does not exist, create it
+// 	}),
+// }))
+
+// server.use(usersRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.get("/", (req, res, next) => {
+    res.send("YOU GOT IT!")
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
