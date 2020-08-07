f511410c5a34de536cec0b14ba04a874e299945f
Carlos 'Mannie' Alvarez <carlos.alvarezberrio@gmail.com>
2020-08-07 00:38:50 -0400

    cleaned up restricted middleware

--- auth/restricted.js
+++ auth/restricted.js
@@ -4,19 +4,20 @@ const secrets = require("../config/secrets.js");
 module.exports = (req, res, next) => {
     if(!req.headers.authorization) {
         res.status(401).json({ message: "You shall not pass!" });
-    }
-    const [authType, token] = req.headers.authorization.split(" ");
-
-    if(authType.toUpperCase() === "BEARER" && token) {
-        jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-            if(!err) {
-                req.jwt = decodedToken;
-                next();
-            } else {
-                res.status(401).json({ message: "You shall not pass!", err });
-            }
-        });
     } else {
-        res.status(401).json({ message: "You shall not pass!" });
+        const [authType, token] = req.headers.authorization.split(" ");
+    
+        if(authType.toUpperCase() === "BEARER" && token) {
+            jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+                if(!err) {
+                    req.jwt = decodedToken;
+                    next();
+                } else {
+                    res.status(401).json({ message: "You shall not pass!", err });
+                }
+            });
+        } else {
+            res.status(401).json({ message: "You shall not pass!" });
+        }
     }
 }
