788e48a709e98d4948c6f6a6f2343a857c0b478c
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-10-31 07:50:48 -0500

    server running/get and post working

--- users/users-model.js
+++ users/users-model.js
@@ -4,6 +4,27 @@ function find() {
     return db('users')
 }
 
+function findBy(filter) {
+     return db('users')
+     .select('id', 'username', 'password')
+     .where(filter)
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user)
+    return findById(id)
+}
+
+function findById(id) {
+    return db('users')
+    .select('id','username')
+    .where({id})
+    .first()
+}
+
 module.exports = {
-    find
+    find,
+    findBy,
+    add,
+    findById
 }
--- users/users-router.js
+++ users/users-router.js
@@ -12,4 +12,28 @@ router.get("/users", async (req,res,next) => {
     }
 })
 
+router.post("/users", async (req,res,next) => {
+    try {
+        const { username, password } = req.body
+		const user = await Users.findBy({ username }).first()
+		
+
+        if (user) {
+            return res.status(409).json({
+                message: 'username taken'
+            })
+        }
+
+        const addUser = await Users.add({
+            username,
+            password
+        })
+
+
+        res.status(201).json(addUser)
+    } catch(err) {
+        next(err)
+    }
+})
+
 module.exports = router
