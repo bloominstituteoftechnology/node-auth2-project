44fe6c0f85f3be386d4bbff78ecf726349f7c887
April Darger <april.m.darger@gmail.com>
2020-12-15 15:55:56 -0700

    Added develpmnet settings for pool, migrations, seeds, and useNullasDefault.

--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,29 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: 'true',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/users.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migration: {
+      directory: './database/migrations'
+    }, 
+    seeds: {
+      directory: './database/seeds'
     }
   },
 
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+
   }
 
 };
