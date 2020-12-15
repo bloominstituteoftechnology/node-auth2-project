d0dee2c4512c4c02e8b2321028af06dc1cf6c9aa
April Darger <april.m.darger@gmail.com>
2020-12-15 14:40:32 -0700

    File structure set up and server set up.

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`** Running on port ${port} **`);
+  console.log(`\n** Running on port ${port} **\n`);
+});
