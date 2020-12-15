da9eac8048741ac6949a9d72e048f2cd5303be38
April Darger <april.m.darger@gmail.com>
2020-12-15 15:00:38 -0700

    Server set up.

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const server = require('./api/server');
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`** Running on port ${port} **`);
   console.log(`\n** Running on port ${port} **\n`);
 });
