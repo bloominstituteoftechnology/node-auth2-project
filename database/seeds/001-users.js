8bc72771b7d2ef9cdaa1c887c30f9950a191d127
April Darger <april.m.darger@gmail.com>
2020-12-15 17:04:36 -0700

    Fixed scripts in package.json, fixed table name in seeds.

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('users').insert([
         {username: 'Paco',password: '11111',department: 'Watching'},
         {username: 'Shiro',password: '22222',department: 'Outside'},
         {username: 'Monkey',password: '33333',department: 'Atack'}
