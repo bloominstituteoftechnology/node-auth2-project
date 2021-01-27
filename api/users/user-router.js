9f1fa964b95783e06ce2b641c11aa84b12a7baed
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2021-01-26 19:56:00 -0500

    2nd commit

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const Users = require('./user-model')
+const jwt = require('jsonwebtoken')
+const { jwtSecret } = require('../auth/secrets')
+const bcryptjs = require('bcryptjs')
 
 router.get('/', (req, res) => {
     Users.get()
@@ -11,9 +14,13 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    Users.add(req.body)
+router.post('/register', (req, res) => {
+const credentials = req.body
+
+    const hash = bcryptjs.hashSync(credentials.password, 10);
+    credentials.password = hash
+
+    Users.add(credentials)
     .then(newUser => {
         console.log(newUser)
         res.status(210).json(newUser)
@@ -24,5 +31,22 @@ router.post('/', (req, res) => {
     })  
 })
 
+router.post('/login', (req, res) => {
+    
+})
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.name,
+        department: user.department
+    }
+
+    const options = {
+        expiresIn: '1d'
+    }
+
+    return jwt.sign(payload, jwtSecret, options)
+}
 
 module.exports = router;
