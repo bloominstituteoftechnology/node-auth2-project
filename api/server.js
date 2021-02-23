95549252dd2ed2331cedc3237e0fecf2234650f7
Diego Roman <diegormnv209@gmail.com>
2021-02-23 12:04:46 -0800

    made scripts and server up and running. amde mor files for routers

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet')
+const cors = require('cors');
+
+const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/users-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+    res.json({ api: 'Server is up and running!' });
+  });
+  
+  module.exports = server;
