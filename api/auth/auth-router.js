c14a4567001473fa45243988bc347f90a67bb588
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-03 22:54:52 +0100

    clarification token

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -27,6 +27,15 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
+
+    The token must expire in one day, and must provide the following information
+    in its payload:
+
+    {
+      "subject"  : 1       // the user_id of the authenticated user
+      "username" : "bob"   // the username of the authenticated user
+      "role_name": "admin" // the role of the authenticated user
+    }
    */
 });
 
