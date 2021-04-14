1a479a54bfd8d7cf3b8f522eb6dfac6b0f20c812
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-04-14 20:35:44 +0200

    deps

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 
 server.use((err, req, res, next) => { // eslint-disable-line
-  res.status(500).json({
+  res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
   });
