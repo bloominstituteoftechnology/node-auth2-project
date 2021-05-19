aa23ef2e96a2356d9fc6bbef0f946318240cb082
Craig Gerhardstein <77256669+Craig-Gerhardstein@users.noreply.github.com>
2021-05-19 00:22:48 -0400

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
+const bcrypt = require('bcryptjs');
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require("../secrets"); // use this secret!
+const Users = require('../users/users-model');
 
 router.post("/register", validateRoleName, (req, res, next) => {
+  let user = req.body;
+
+  const rounds = process.env.BCRYPT_ROUNDS || 8;
+  const hash = bcrypt.hashSync(user.password, rounds);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(next);
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
     response:
     status 201
     {
@@ -18,19 +32,26 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  let {password, user} = req.body;
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = makeToken(user);
+    res.status(200).json({
+      message: `${user.username} is back!`,
+      token
+    })
+  } else {
+    next({message: "Invalid Credentials", status: 401});
+  }
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
     response:
     status 200
     {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
-
     The token must expire in one day, and must provide the following information
     in its payload:
-
     {
       "subject"  : 1       // the user_id of the authenticated user
       "username" : "bob"   // the username of the authenticated user
@@ -38,5 +59,17 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
     }
    */
 });
+
+function makeToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  }
+  const options = {
+    expiresIn: "1d"
+  }
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
