f77152a754b0c71a5d0a5994bc56819ce25d3dbb
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-06-15 19:50:12 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const {findBy} = require('../users/users-model')
+const jwt = require('jsonwebtoken')
 
 const restricted =  (req, res, next) => {
   /*
@@ -17,6 +18,23 @@ const restricted =  (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+    const token = req.headers.authorization
+    if(!token){
+      return next({status:401, message: 'Token required'})
+    }
+    jwt.verify(token, JWT_SECRET, (err, decodedToken)=>{
+      if(err){
+        next({ status: 401,
+        message: 'Token required'})
+      }else{
+        req.decodedToken = decodedToken
+        next()
+
+      }
+
+    })
+
+    
 
 }
 
