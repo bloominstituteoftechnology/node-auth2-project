cd55f1a0051627c27728c0b34e1382d521034f9b
yonathan Admasu <admyoni4@yahoo.com>
2021-07-10 07:42:59 -0700

    finally done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const { findBy } = require('../users/users-model')
-
+const jwt = require('jsonwebtoken')
 const restricted = (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
@@ -17,7 +17,18 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
- 
+ const token = req.headers.authorization
+ if (!token) {
+  return next({ status:401, message: 'Token required'})
+ }
+ jwt.verify(token, JWT_SECRET, (err,decodedToken) =>{
+   if (err) {
+     next({ status: 401, message: 'Token invalid'})
+   }else {
+     req.decodedToken = decodedToken
+     next()
+   }
+ })
 }
 
 const only = role_name => (req, res, next) => {
@@ -31,7 +42,11 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
- next()
+ if (role_name === req.decodedToken.role_name) {
+   next()
+ }else {
+   next({ status: 403, message: 'This is not for you' })
+ }
 }
 
 
