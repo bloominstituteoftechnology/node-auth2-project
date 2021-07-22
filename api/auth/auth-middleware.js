2f7af823e69ff9846e468ca3223238231bfdd04a
XavierTwitty <x.twitty@gmail.com>
2021-07-21 22:59:47 -0400

    done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,6 @@
 const { jwtSecret } = require("../secrets"); // use this secret!
 const { findBy } = require("../users/users-model");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const restricted = (req, res, next) => {
   /*
@@ -19,19 +19,18 @@ const restricted = (req, res, next) => {
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
 
-  const token =  req.headers.authorization 
+  const token = req.headers.authorization;
   if (!token) {
-    return next({ status: 401 , message: "token required"})
-  } 
-  jwt.verify(token , jwtSecret, (err, decodedToken) => {
+    return next({ status: 401, message: "token required" });
+  }
+  jwt.verify(token, jwtSecret, (err, decodedToken) => {
     if (err) {
-      next({ status: 401 , message: 'Token invalid'})
+      next({ status: 401, message: "Token invalid" });
     } else {
-      req.decodedToken = decodedToken
-      next()
-
+      req.decodedToken = decodedToken;
+      next();
     }
-  })
+  });
 };
 
 const only = (role_name) => (req, res, next) => {
@@ -45,7 +44,11 @@ const only = (role_name) => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-  next();
+  if ((role_name === req.decodedToken.role_name)) {
+    next();
+  } else {
+    next({ status: 403, message: "This is not for you" });
+  }
 };
 
 const checkUsernameExists = async (req, res, next) => {
@@ -61,7 +64,7 @@ const checkUsernameExists = async (req, res, next) => {
     if (!user) {
       next({ status: 422, message: "Invalid credentials" });
     } else {
-      req.user = user
+      req.user = user;
       next();
     }
   } catch (err) {
