7f0f0f9215b164c12aba31da1099a38f7edda09f
tamara-taylor <ttaylor8522@gmail.com>
2021-10-30 18:32:16 -0400

    half passing

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -6,8 +6,6 @@
   If no fallback is provided, TESTS WON'T WORK and other
   developers cloning this repo won't be able to run the project as is.
  */
-
-const JWT_SECRET = process.env.JWT_SECRET || 'shhh'
 module.exports = {
-  JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET || 'shh'
 }
