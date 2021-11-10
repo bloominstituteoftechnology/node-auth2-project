268c98ababea7ed82dfcfef5c627aa93f5bbdf58
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-10 06:18:58 -0800

    adjusted token-builder

--- api/auth/token-builder.js
+++ api/auth/token-builder.js
@@ -3,14 +3,15 @@ const { JWT_SECRET } = require("./../secrets")
 
 const buildToken = (user) => {
   const payload = {
-    subject: user.id,
+    subject: user.user_id,
     username: user.username,
-    role_id: user.role,
+    role_id: user.role_name,
   }
   const options = {
     expiresIn: "1d",
   }
-  return jwt.sign(payload, JWT_SECRET, options)
+  const signedToken = jwt.sign(payload, JWT_SECRET, options)
+  return signedToken;
 }
 
 module.exports = buildToken;
