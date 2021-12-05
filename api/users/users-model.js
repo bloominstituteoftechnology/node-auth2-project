f6e183eaaa1b7657a1c2abeed6712d513263b283
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-12-04 17:28:10 -0800

    findById completed

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -54,6 +54,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'role_name')
+      .where('users.user_id', user_id)
+      .first()
 }
 
 /**
