17600a0acdbd4bec2a5ec1ece7d0dccfb8d0ff61
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-12-04 17:17:09 -0800

    find completed

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,9 @@ function find() {
       }
     ]
    */
+    return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
