0c67225e9b17afa3db9859da037e7380541b5d87
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-12-04 17:55:27 -0800

    auth router register completed

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -76,6 +76,7 @@ const validateRoleName = (req, res, next) => {
         message: 'Role name can not be longer than 32 characters'
       })
     }else {
+      req.role_name = req.body.role_name.trim()
       next()
     }
   }
