23e7c82bede478a02fa63aa21ef7842efeefb699
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-12-04 16:03:02 -0800

    changed index.js

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -5,7 +5,8 @@
 
   If no fallback is provided, TESTS WON'T WORK and other
   developers cloning this repo won't be able to run the project as is.
- */
+*/
+const JWT_SECRET = process.env.JWT_SECRET || 'shh'
 module.exports = {
-
+  JWT_SECRET
 }
