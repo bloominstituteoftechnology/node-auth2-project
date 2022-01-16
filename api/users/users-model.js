1b31f0a85e44dca7e0599085b9c1de1c35997594
unknown <jlarson129@outlook.com>
2022-01-16 11:55:23 -0500

    users model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,6 +1,9 @@
 const db = require('../../data/db-config.js');
 
 function find() {
+  return db("users as u")
+    .join("roles as r", "u.role_id", "=", "r.role_id")
+    .select("u.user_id","u.username","r.rolename")
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
@@ -21,6 +24,10 @@ function find() {
 }
 
 function findBy(filter) {
+  return db("users as u")
+    .join("roles as r", "u.role_id", "=", "r.role_id")
+    .select("u.user_id","u.username","u.password","r.role_name")
+    .where(filter)
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
@@ -37,6 +44,11 @@ function findBy(filter) {
 }
 
 function findById(user_id) {
+  return db("users as u")
+    .join("roles as r", "u.role_id", "=", "r.role_id")
+    .select("u.user_id","u.username","r.role_name")
+    .where("u.user_id", user_id)
+    .first()
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
