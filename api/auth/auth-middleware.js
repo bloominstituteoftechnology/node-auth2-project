786d6e5506ed7c12b598f5657e8c0bfe829421d4
joey-bertschler <joey.bertschler@gmail.com>
2022-01-17 07:36:35 +0400

    wip2

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -16,6 +16,7 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+ next()
 }
 
 const only = role_name => (req, res, next) => {
