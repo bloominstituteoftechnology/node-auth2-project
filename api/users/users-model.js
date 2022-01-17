314ceaceb048d2178428efc2a084cc84d581efeb
joey-bertschler <joey.bertschler@gmail.com>
2022-01-17 07:50:03 +0400

    wip4

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -79,6 +79,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password','role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
