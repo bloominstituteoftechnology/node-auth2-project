f2046205323afbb2ee9303f94d86e23b78f0443a
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-01-17 19:52:17 -0600

    done with auth-router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -7,7 +7,7 @@ const User = require('../users/users-model')
 
 router.post("/register", validateRoleName, (req, res, next) => {
   
-  const user = req.body
+  let user = req.body
   const hash = bcrypt.hashSync(user.password, 8)
   user.password = hash
   User.add(user)
@@ -19,25 +19,30 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
+  
+  let { username, password } = req.body
+  
+  User.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = makeToken(user)
+        res.status(200).json({message: `Welcome back ${user.username}`, token})
+      } else {
+        next({status: 401, message: 'Invalid credential'})
     }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
+  }).catch(next)
 });
+
+function makeToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role: user.role_name
+  }
+  const options = {
+    expiresIn: "24h"
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
