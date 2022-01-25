9926598082658b3b6b6c412c52600245179ca269
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-25 16:41:03 -0500

    set up project and built out user model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,10 @@ function find() {
       }
     ]
    */
+
+    return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', 'r.role_name as role')
 }
 
 function findBy(filter) {
@@ -34,6 +38,11 @@ function findBy(filter) {
       }
     ]
    */
+
+    return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', 'r.role_name as role', 'u.password')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +56,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', 'r.role_name as role')
+    .where('u.id',user_id)
+    .first()
 }
 
 /**
