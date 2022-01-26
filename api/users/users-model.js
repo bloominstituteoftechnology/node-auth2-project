e20716e2947e323b8871db77c15cf878cf9532e7
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-25 19:58:29 -0500

    updated model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -59,7 +59,7 @@ function findById(user_id) {
     return db('users as u')
     .join('roles as r', 'u.role_id', '=', 'r.role_id')
     .select('u.user_id', 'u.username', 'r.role_name as role')
-    .where('u.id',user_id)
+    .where('u.user_id',user_id)
     .first()
 }
 
